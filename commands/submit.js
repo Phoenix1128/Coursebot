f2f6babe80c7ebe8633f8a4469413ac301abfc95
Phoenix1128 <pdinsdale1128@gmail.com>
2019-07-20 16:33:05 -0400

    Disabling submit cmd

--- commands/submit.js
+++ commands/submit.js
@@ -5,6 +5,7 @@ module.exports = {
   aliases: ['sub'],
   usage: '[code] "[title]" "(description)" "(misc. info)"\nEverything in () is optional. Everything in [] is mandatory',
   args: '[code] => The course ID you wish to share, dashes included. Ex. XXX-XXX-XXX\n"[title]" => The title of the course\n"(description)" => The description of the course\n"(misc. info)" => Any other infomation you would like t include such as the game style of the course, the theme, or any tags. Please keep this field to a character minimum to visually improve the embeds.\n\nThe quotation marks are very important!',
+  enabled: false,
   async run(client, message, args) {
     client.insertLevel(message, args, 'submit');
   },
